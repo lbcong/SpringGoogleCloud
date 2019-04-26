75aab6392d73f3a6478ee20000917c2f9f9a65c4
Your Name <you@example.com>
2019-04-27 00:37:04 +0700

    update1

--- src/main/java/com/spring/Demo/ServletInitializer.java
+++ src/main/java/com/spring/Demo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.Demo;
+package com.spring.Demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
